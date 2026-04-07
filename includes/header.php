a458744d6a3808a1ad9ef522f4332a4b4aa6fe31
Byd1ego <javicastello2003@gmail.com>
2026-04-07 14:36:15 +0200

    Añado la estructura y el logo

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div>
+            <img src="./static/img/Logo.png" alt="" height="200" width="200">
+        </div>
+        <div>
+            <ul>
+                <li><a href="index.php"></a>Inicio</li>
+                <li><a href="contacto.php"></a>Contacto</li>
+                
+            </ul>
+            <button><a href="Login"></a>Login</button>
+        </div>
+    </header>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include_once 'includes/header.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="static/css/estilos.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
